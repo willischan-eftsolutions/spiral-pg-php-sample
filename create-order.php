f402cc99761fc22701b6f3ec4f7756c93f5e5103
willischan-eftsolutions <71488419+willischan-eftsolutions@users.noreply.github.com>
2020-09-29 13:58:33 +0800

    Add files via upload
    Initial Release

---
+++ create-order.php
@@ -0,0 +1,56 @@
+<?php
+	require_once 'vendor/autoload.php';
+	
+	use GuzzleHttp\Client;
+	   
+	$merchantRef = (string) (round(microtime(true) * 1000));
+	//$url = "http://127.0.0.1:3000/merchants/000000000000001/transactions/" . $merchantRef;
+	$url = "https://cjpazdufok.execute-api.ap-east-1.amazonaws.com/v1/merchants/000000000000001/transactions/" . $merchantRef; 
+	$data = json_encode([
+		"clientId" => "000000000000001",
+		"cmd" => "SALESESSION",
+		"type" => "VM",
+		"amt" => 1.00,
+		"merchantRef" => $merchantRef,
+		"channel" => "WEB",
+		"successUrl" => "http://dummyhost/mpgstestsuccess.html",
+		"failureUrl" => "http://dummyhost/mpgstestfail.html",
+		"webhookUrl" => "http://dummyhost/webhook",
+		"goodsName" => "Goods Name"
+	]);
+	$now = new Datetime("now");
+	$now->setTimeZone(new DateTimeZone('UTC'));
+	$request_datetime = $now->format('Y-m-d\TH:i:s\Z');
+	$payload = "000000000000001" . $merchantRef . $request_datetime;
+	// create digital signature
+	$pkeyid = openssl_pkey_get_private("file://./custpri.pem");
+	openssl_sign($payload, $signature, $pkeyid, OPENSSL_ALGO_SHA256);
+	openssl_free_key($pkeyid);
+		
+	// initiate request
+	$client = new Client();
+	$response = $client->request('PUT', $url, [
+		'body' => $data,
+		'headers' => [
+			'Content-Type' => 'application/json',
+			//'Spiral-Client-Id' => '000000000000001',
+			'Spiral-Request-Datetime' => $request_datetime,
+			'Spiral-Client-Signature' => base64_encode($signature)
+		]
+	]);
+	
+	// check server signature
+	$pubkeyid = openssl_pkey_get_public("file://./spiralpub.pem");
+	$server_sig_payload = "000000000000001" . $merchantRef . $response->getHeader('Spiral-Request-Datetime')[0];
+	$ok = openssl_verify($server_sig_payload, base64_decode($response->getHeader('Spiral-Server-Signature')[0]), $pubkeyid, OPENSSL_ALGO_SHA256);
+	if ($ok == 1) {
+		$response_body = json_decode($response->getBody());
+		echo $response_body->sessionId;
+	} elseif ($ok == 0) {
+		echo "bad";
+	} else {
+		echo "ugly, error checking signature";
+	}
+	openssl_free_key($pubkeyid);
+	
+?>
---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+	<head>
+		<script src="https://d2tgyn2qxlrcd9.cloudfront.net/js/v1/spiralpg.min.js"></script>
+		<!--
+		<script src="./spiralpg.min.js"></script>
+        -->
+		<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+		<script type="text/javascript">
+			function createOrder() {
+				axios.get('./create-order.php')
+					.then(function (response) {
+						document.getElementById('sessionId').innerHTML = response.data;
+						SpiralPG.init(response.data);
+					})
+					.catch(function (error) {
+						console.log(error);
+					});
+			}
+			
+			function pay() {
+				try {
+					SpiralPG.pay();
+					setTimeout("showButton();", 2000);
+				} catch (err) {
+					document.getElementById("loading").innerHTML = "processing...";
+					setTimeout("pay();", 200);
+				}
+			}
+			
+			function showButton() {
+				try {
+					document.getElementById("loading").innerHTML = "Please click below button to proceed if the page is not been redirected automatically. <br>";
+					document.getElementById("loading").innerHTML += "<input type=\"button\" value=\"Click here to proceed.\" onclick=\"pay();\"><br>";
+				} catch (err) {
+					document.getElementById("loading").innerHTML = err.message;
+				}
+			}
+		</script>
+    </head>
+    <body onload="createOrder()">
+	  <input type="button" value="Create Order" onclick="createOrder();" />
+	  <br/>
+	  <p><b>Session ID: </b><span id="sessionId" /></p>
+	  <br/>
+      <input type="button" value="Pay with SpiralPG" onclick="pay();" />
+	  <br/>
+	  <div id="loading"></div>
+    </body>
+</html>
